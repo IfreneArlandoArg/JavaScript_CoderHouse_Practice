7057112d8dd9c349ecabc92b32805fcc2546b34d
Arlando Ifrene <aifrene@quadiontech.com>
2025-02-04 08:30:15 -0300

    Agregar libros, Listar librose, con menu...

---
+++ week_3_pract/pract_2/main.js
@@ -0,0 +1,71 @@
+function SchemaLibro(Autor, Titulo)
+{
+    return {Autor, Titulo}
+}
+
+function AgregarLibro(Autor, Titulo)
+{
+    libros.push(SchemaLibro(Autor, Titulo));
+
+    console.log(`¡${Titulo}, Agregado!`);
+
+    
+}
+
+function UsuarioAgregarLibro()
+{
+    let autor = prompt("Ingresé Nombre del Autor :");
+    let titulo = prompt("Ingresé Titulo del Libro :");
+
+    AgregarLibro(autor, titulo);
+}
+
+function MostrarLibro(Libro)
+{
+    return `Autor : ${Libro.Autor}, Titulo : ${Libro.Titulo}.`;   
+}
+
+function ListarLibros(Libros)
+{
+    let textoAlerta = "";
+    Libros.forEach(libro => {
+        textoAlerta += MostrarLibro(libro) + "\n";
+    });
+
+    alert(textoAlerta);
+
+}
+
+
+let libros = [
+    {Autor : "J.K. Rowling", Titulo :"Harry Potter"}
+];
+
+
+
+
+let menu = parseInt(prompt(`Elige Opereción :\n1 - Agregar Libro. \n2 - Listar Libros. \n3 - Salir..`));
+
+
+while (menu !== 3) {
+
+    switch (menu) 
+    {
+        case 1:
+            UsuarioAgregarLibro();
+            break;
+        case 2:
+            ListarLibros(libros);
+            break;
+        default :
+            alert(`Numero fuera de rango de opciones posibles...`);
+            break;
+
+    
+    }
+
+
+
+    menu = parseInt(prompt(`Elige Opereción :\n1 - Agregar Libro. \n2 - Listar Libros. \n3 - Salir..`)); 
+
+}
