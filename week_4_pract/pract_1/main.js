ebd9b06f1fae2fb45eb8b74d48bf9c792c6d6dde
Arlando Ifrene <aifrene@quadiontech.com>
2025-02-08 11:33:52 -0300

    Added functions to filter even and odd numbers from an array and updated logging statement

--- week_4_pract/pract_1/main.js
+++ week_4_pract/pract_1/main.js
@@ -10,5 +10,38 @@ function generateRandomArray(size, min, max) {
     return arr.sort(() => Math.random() - 0.5); // Shuffle the array
 }
 
+//Returns an array even numbers, from an array (received).
+//Returns an array of empty numbers, in case the array is empty, or if there are no even numbers.
+function getEvenNumbers(arr){
+    let evenNumbers = [];
+
+    arr.foreach(x =>{
+        if(x % 2 === 0){
+            evenNumbers.push(x);
+        }
+    });
+  
+    return evenNumbers;
+
+}
+
+
+
+//Returns an array odd numbers, from an array (received).
+//Returns an array of empty numbers, in case the array is empty, or if there are no odd numbers.
+function getOddNumbers(arr){
+    let oddNumbers = [];
+
+    arr.foreach(x =>{
+        if(x % 2 !== 0){
+            oddNumbers.push(x);
+        }
+    });
+  
+    return oddNumbers;
+}
+
+
+
 const randomArray = generateRandomArray(10, 1, 100); // Example: 10 random integers between 1 and 100
-console.log(randomArray);
+console.log(`Random integers between 1 and 100 : \n ${randomArray}`);
