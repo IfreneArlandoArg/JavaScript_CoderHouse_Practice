3c53e0061c8b174854be7a48fd016eb60c1b0601
Arlando Ifrene <aifrene@quadiontech.com>
2025-02-09 09:38:14 -0300

    Created the main functions on which the features of the program is based, and created the main prompt menu...

--- week_4_pract/pract_1/main.js
+++ week_4_pract/pract_1/main.js
@@ -10,7 +10,7 @@ function generateRandomArray(size, min, max) {
     return arr.sort(() => Math.random() - 0.5); // Shuffle the array
 }
 
-//Returns an array even numbers, from an array (received).
+//Returns an array of even numbers, from an array (received).
 //Returns an array of empty numbers, in case the array is empty, or if there are no even numbers.
 function getEvenNumbers(arr){
     let evenNumbers = [];
@@ -25,9 +25,7 @@ function getEvenNumbers(arr){
 
 }
 
-
-
-//Returns an array odd numbers, from an array (received).
+//Returns an array of odd numbers, from an array (received).
 //Returns an array of empty numbers, in case the array is empty, or if there are no odd numbers.
 function getOddNumbers(arr){
     let oddNumbers = [];
@@ -41,7 +39,31 @@ function getOddNumbers(arr){
     return oddNumbers;
 }
 
+//Receives an array of numbers, and returns an array of numbers (increasing order)....
+function sortArrayIncreasingOrder(arr){
+   arr.sort((a,b) => a - b);
+   return arr;
+}
+
+
+//Receives an array of numbers, and returns an array of numbers (decreasing order)....
+function sortArrayDecreasingOrder(arr){
+    arr.reverse(sortArrayIncreasingOrder(arr));
+    return arr;
+}
+
+function fnMenu(rndArray){
+
+return parseInt(prompt(`Choose operation to be executed on the following array \n\[ ${rndArray.join(" - ")} \] : \n
+1 - Get Even numbers.\n2 - Get odd numbers.\n3 - Sort in increasing order.\n4 - Sort in decreasing order.\n5 - Exit.`))
+}
 
 
 const randomArray = generateRandomArray(10, 1, 100); // Example: 10 random integers between 1 and 100
-console.log(`Random integers between 1 and 100 : \n ${randomArray}`);
+console.log(randomArray);
+
+
+let menu = fnMenu(randomArray);
+
+console.log(menu);
+
