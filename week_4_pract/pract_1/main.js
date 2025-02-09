a909cb4a536fd983c62febac7c58aac4eb8a5ac8
Arlando Ifrene <aifrene@quadiontech.com>
2025-02-09 10:51:32 -0300

    Debugging and implementation of Use cases in recurring (loop) menu...

--- week_4_pract/pract_1/main.js
+++ week_4_pract/pract_1/main.js
@@ -14,13 +14,15 @@ function generateRandomArray(size, min, max) {
 //Returns an array of empty numbers, in case the array is empty, or if there are no even numbers.
 function getEvenNumbers(arr){
     let evenNumbers = [];
+    
 
-    arr.foreach(x =>{
+    arr.forEach(x => {
         if(x % 2 === 0){
             evenNumbers.push(x);
         }
     });
-  
+
+    
     return evenNumbers;
 
 }
@@ -30,7 +32,7 @@ function getEvenNumbers(arr){
 function getOddNumbers(arr){
     let oddNumbers = [];
 
-    arr.foreach(x =>{
+    arr.forEach(x =>{
         if(x % 2 !== 0){
             oddNumbers.push(x);
         }
@@ -52,10 +54,15 @@ function sortArrayDecreasingOrder(arr){
     return arr;
 }
 
-function fnMenu(rndArray){
+function optionMenu(rndArray){
 
 return parseInt(prompt(`Choose operation to be executed on the following array \n\[ ${rndArray.join(" - ")} \] : \n
 1 - Get Even numbers.\n2 - Get odd numbers.\n3 - Sort in increasing order.\n4 - Sort in decreasing order.\n5 - Exit.`))
+}
+
+function showJoinedArray(operation, arr){
+    console.log(`${operation} \[ ${arr.join(" - ")} \]`);
+    alert(`${operation} \[ ${arr.join(" - ")} \]`);
 }
 
 
@@ -63,7 +70,38 @@ const randomArray = generateRandomArray(10, 1, 100); // Example: 10 random integ
 console.log(randomArray);
 
 
-let menu = fnMenu(randomArray);
+let menu = optionMenu(randomArray);
 
-console.log(menu);
+while(menu !== 5){
+    switch(menu){
+        case 1:
+            let evenNumbers = getEvenNumbers(randomArray);
+            showJoinedArray("Even numbers :", evenNumbers);
+
+            break;
+        case 2:
+            let oddNumbers =  getOddNumbers(randomArray);
+            showJoinedArray("Odd numbers :", oddNumbers);
+
+            break;
+        case 3:
+            let sortedArrayIncreasingOrder = sortArrayIncreasingOrder(randomArray);
+            showJoinedArray("Sorted Array Increasing Order :", sortedArrayIncreasingOrder);
+
+            break;
+        case 4:
+            let sortedArrayDecreasingOrder =  sortArrayDecreasingOrder(randomArray);
+            showJoinedArray("Sorted Array Decreasing Order :", sortedArrayDecreasingOrder);
+
+            break;
+        default:
+            alert(`Option ${menu} out of range.\nPlease choose a valid option.`);
+            break;
+    }
+
+    menu = optionMenu(randomArray);
+
+}
+
+
 
