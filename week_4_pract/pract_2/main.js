5dba668b31d9382d8292775beb97352f630e2828
Arlando Ifrene <aifrene@quadiontech.com>
2025-02-14 11:13:12 -0300

    Added birthdate and age calculation to User class

--- week_4_pract/pract_2/main.js
+++ week_4_pract/pract_2/main.js
@@ -2,25 +2,37 @@
 
 
 //Functions
+function calculateAge(birthdateString){
+    let today = new Date();
+    let birthdate = new Date(birthdateString);
+    
+    return today.getFullYear() - birthdate.getFullYear();
+
+
+}
 
 
 //Classes
 class User {
 
-    constructor(name, dni, phone){
+    constructor(name, dni, phone, birthdateString){
         this.name = name;
         //birthdate...
         //Calculates age using an external fn that takes birthdate as input.
+        this.birthdate = new Date(birthdateString);
+        this.age = calculateAge(birthdateString);
         this.dni = dni;
         this.phone = phone;
+        
     }
 
     getInfos(){
-      return  `User : ${this.name}.\n DNI: ${this.dni}.\n Phone: ${this.phone}.`;
+      return  `User : ${this.name}.\nDNI: ${this.dni}.\nAge : ${this.age}\nPhone: ${this.phone}.\nBirthDate : ${this.birthdate}`;
     }
 
 
 }
+
 
 
 
@@ -35,15 +47,17 @@ do{
     let name = prompt("Enter user name :");
     let dni = prompt("Enter user DNI :");
     let phone = prompt("Enter user phone :");
+    let birthdateString = prompt("Enter user birthdate (mm/dd/yyyy) :");
 
-    let tempUser = new User(name, dni, phone);
+    let tempUser = new User(name, dni, phone, birthdateString);
 
     console.log(tempUser.getInfos());
+    
 
     users.push(tempUser);
     
 
-    addMoreUser = parseInt(prompt("Do you want to add more user(s)? \n 1 - Yes \n2 - No."));
+    addMoreUser = parseInt(prompt("Do you want to add more user(s)? \n1 - Yes \n2 - No."));
 
 }
 while(addMoreUser === 1)
